1dac96e74ac0ae336a05e0f9ef1d597c7bed3416
long <wang_hailong1977@hotmail.com>
2023-01-05 14:00:19 +0800

    更改了材质相关的一个bug

--- Source/RuntimeSkeletalMeshGenerator/RuntimeSkeletalMeshGenerator.cpp
+++ Source/RuntimeSkeletalMeshGenerator/RuntimeSkeletalMeshGenerator.cpp
@@ -147,12 +147,21 @@ void FRuntimeSkeletalMeshGenerator::GenerateSkeletalMesh(
 	}
 	check(ImportedModelData.PointToRawMap.Num() == Vertices.Num());
 
-	for (int32 I = 0; I < Surfaces.Num(); I++)
+
+	for (auto& s : Surfaces)
 	{
-		SkeletalMeshImportData::FMaterial& Mat = ImportedModelData.Materials.AddDefaulted_GetRef();
-		Mat.Material = SurfacesMaterial[I];
-		Mat.MaterialImportName = SurfacesMaterial[I]->GetFullName();
+		//这里材质的赋值不一定完全正确，不知道如果SurfacesMaterial为空的情况下是否会出错
+		if (s.MaterialIndex >= 0 && s.MaterialIndex < SurfacesMaterial.Num())
+		{
+			if (s.MaterialIndex >= ImportedModelData.Materials.Num())
+			{
+				SkeletalMeshImportData::FMaterial& Mat = ImportedModelData.Materials.AddDefaulted_GetRef();
+				Mat.Material = SurfacesMaterial[s.MaterialIndex];
+				Mat.MaterialImportName = SurfacesMaterial[s.MaterialIndex]->GetFullName();
+			}
+		}
 	}
+
 
 	ImportedModelData.Faces.SetNum(Indices.Num() / 3);
 	for (int32 FaceIndex = 0; FaceIndex < ImportedModelData.Faces.Num(); FaceIndex += 1)
@@ -301,7 +310,7 @@ void FRuntimeSkeletalMeshGenerator::GenerateSkeletalMesh(
 		RenderSection.NumVertices = Surface.Vertices.Num();
 		RenderSection.BaseIndex = SurfaceIndexOffsets[I];
 		RenderSection.NumTriangles = Surface.Indices.Num() / 3;
-		RenderSection.MaterialIndex = I;
+		RenderSection.MaterialIndex = Surfaces[I].MaterialIndex;
 		RenderSection.bCastShadow = true;
 		RenderSection.bRecomputeTangent = false;
 		RenderSection.MaxBoneInfluences = MaxBoneInfluences;
